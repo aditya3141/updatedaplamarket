d9a329e5c29513b738576e088b995e8ffa07db3d
aditya3141 <118032727+aditya3141@users.noreply.github.com>
2024-06-30 18:54:33 +0530

    test
    test

--- src/Pages/auth/Login.js
+++ src/Pages/auth/Login.js
@@ -19,10 +19,13 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post(`/api/v1/auth/login`, {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        `https://backend-market-1bby.onrender.com/api/v1/auth/login`,
+        {
+          email,
+          password,
+        }
+      );
       if (res && res.data.success) {
         toast.success(res.data.message);
         setAuth({
